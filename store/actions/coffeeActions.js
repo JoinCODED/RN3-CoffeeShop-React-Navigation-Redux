31b3e4bbc844d1d834d9e1db23c30e98a89e089e
Lailz <lailaabdulraheem@gmail.com>
2019-03-17 02:08:38 +0300

    fixed store

--- store/actions/coffeeActions.js
+++ store/actions/coffeeActions.js
@@ -4,19 +4,18 @@ import axios from "axios";
 import * as actionTypes from "./types";
 
 // Get all coffeeShops
-export const getCoffeeShops = async () => dispatch => {
+export const getCoffeeShops = () => async dispatch => {
   dispatch(setCoffeeShopsLoading());
-  try{
+  try {
     let res = await axios.get("http://coffee.q8fawazo.me/api/?format=json");
     let coffeeshops = res.data;
     dispatch({
       type: actionTypes.GET_COFFEESHOPS,
       payload: coffeeshops
     });
-  } catch(err) {
-    console.error("Error while fetching coffeeshops", err)
+  } catch (err) {
+    console.error("Error while fetching coffeeshops", err);
   }
-    
 };
 
 // Set the loading state
